353ca790c5b222efcdc4dc9d648321e2d69ff01f
facsert <facsert@outlook.com>
2024-12-25 22:18:44 +0800

    Todo
    1.chekc tuari build config
    2.add github workflow
    
    Signed-off-by: facsert <facsert@outlook.com>

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,8 @@ import {
 import {
   Dialog,
   DialogContent,
-  DialogHeader,
   DialogTitle,
   DialogTrigger,
-  DialogFooter,
   DialogClose,
 } from "@/components/ui/dialog";
 import {
@@ -41,23 +39,17 @@ class Task {
 
 type Filter = 'all' | 'done' | 'undone'
 
-
-
 export default function Home() {
-  const [tempTask, setTempTask] = useState<Task>(new Task());
   const [tasks, setTasks] = useState<Task[]>([]);
   const [filter, setFilter] = useState<Filter>('all');
+  
+  const [editTask, setEditTask] = useState<Task>(new Task());
+  const [open, setOpen] = useState(false)
   
   useEffect(() => {
     const tasks = localStorage.getItem('tasks');
     if (tasks) {
       setTasks(JSON.parse(tasks));
-    } else {
-      setTasks([
-        { id: new Date(), title: '吃饭', content: '去吃饭', done: false },
-        { id: new Date(), title: '睡觉', content: '去吃饭', done: false },
-        { id: new Date(), title: '编程', content: '去吃饭', done: false },
-      ]);
     }
   }, []);
 
@@ -83,94 +75,114 @@ export default function Home() {
     localStorage.setItem('tasks', JSON.stringify(newTasks));
   }
 
-  const editTask = () => {
-    setTasks(tasks.map(task => task.id === tempTask.id? {...task, ...tempTask} : task))
+  const onSave = () => {
+    setTasks(tasks.map(task => task.id === editTask.id? {...task, ...editTask} : task))
+    toast.success('修改成功');
   }
 
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center">
-      <div>
+    <div className="w-1/2 flex-col justify-center">
+
+      <div className='grid place-items-center mb-8'>
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
           TODO LIST ({tasks.filter(task => !task.done).length}/{tasks.length})
         </h1>
       </div>
 
-      <ToggleGroup type='single' size="lg" className='grid grid-cols-3 gap-0'>
+      <ToggleGroup type='single' size="lg" className='w-full grid grid-cols-3 gap-0 px-auto'>
         <ToggleGroupItem value="all" onClick={() => setFilter('all')}>全部</ToggleGroupItem>
         <ToggleGroupItem value="done" onClick={() => setFilter('done')} >完成</ToggleGroupItem>
         <ToggleGroupItem value="undone" onClick={() => setFilter('undone')}>未完成</ToggleGroupItem>
       </ToggleGroup>
 
-      <div>
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button variant="ghost"> + 添加任务</Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogTitle>Add Todo</DialogTitle>
-            <form action={addTask}>
-              <Label>Title</Label>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline" className='w-full mt-4 mb-4'>添加任务</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogTitle>添加任务</DialogTitle>
+          <form action={addTask} className='flex flex-col gap-4'>
+            <div>
+              <Label>标题</Label>
               <Input name="title" />
-              <Label>Content</Label>
-              <pre>
-                <Textarea name="content" />
-              </pre>
-              <div>
-                <Button type="submit" variant="ghost">添加</Button>
-                <DialogClose>关闭</DialogClose>
-              </div>
-            </form>
-          </DialogContent>
-        </Dialog>
-      </div>
-      <div>
-        <Command>
-          <CommandInput></CommandInput>
-          <CommandList>
-            <CommandEmpty>空</CommandEmpty>
-            {tasks.filter(task => filter === 'all' || (filter === 'done' && task.done) || (filter === 'undone' && !task.done)).map((task, index) => (
-              <CommandItem key={index} onSelect={() => null}>
-                <Checkbox className="mr-2" checked={task.done} onCheckedChange={() => {switchTask(task.id)}} />  
-                <Label className={task.done? "line-through": ""}>
-                  {task.title}
-                </Label>
-                <CommandShortcut className="h-full flex flex-row items-center gap-0">
-                  <Dialog >
-                    <DialogTrigger asChild>
-                      <Button variant="ghost" onClick={() => setTempTask(task)}>
-                        <PenLine className="mr-2 h-6 w-6" />
-                      </Button>
-                    </DialogTrigger>
-                    <DialogContent>
-                      <DialogHeader>
-                        <DialogTitle>Edit Todo</DialogTitle>
-                      </DialogHeader>
-                      
-                      <form action={editTask}>
-                        <Label>Title</Label>
-                          <Input name="title" value={tempTask.title} onChange={(e) => setTempTask({...tempTask, title: e.target.value})} />
-                        <Label>Content</Label>
-                        <Textarea name="content" value={tempTask.content} onChange={(e) => setTempTask({...tempTask, content: e.target.value})} />
-                        
-                        <DialogFooter>
-                          <Button type="submit" variant="ghost">保存</Button>
-                        </DialogFooter>
-                        <div>
-                          <Button type="submit" variant="ghost">保存</Button>
-                          <DialogClose>关闭</DialogClose>
-                        </div>
-                      </form>
-                    </DialogContent>
-                  </Dialog>
-                  <Button variant="ghost" onClick={() => deleteTask(task.id)}>
-                    <Trash2 className="h-6 w-6" />
-                  </Button>
-                </CommandShortcut>
-              </CommandItem>
-            ))}
-          </CommandList>
-        </Command>
-      </div>
+            </div>
+            <div>
+              <Label>任务</Label>
+              <Textarea name="content" className='h-[20vh]' />
+            </div>
+            <div className='grid grid-cols-2 gap-4'>
+              <Button type="submit" variant="outline">添加</Button>
+              <DialogClose asChild>
+                <Button type="button" variant="outline">关闭</Button>
+              </DialogClose>
+            </div>
+          </form>
+        </DialogContent>
+      </Dialog>
+
+      <Dialog 
+        open={open}
+        onOpenChange={() => {setOpen(false)}}
+      >
+        <DialogContent>
+          <DialogTitle>编辑任务</DialogTitle>
+          <form action={onSave} className='flex flex-col gap-4'>
+            <div>
+              <Label>标题</Label>
+              <Input
+                name="title"
+                defaultValue={editTask.title}
+                value={editTask.title}
+                onChange={(e) => setEditTask({...editTask, title: e.target.value})}
+              />
+            </div>
+            <div>
+              <Label>任务</Label>
+              <Textarea
+                name="content"
+                className='h-[20vh]'
+                defaultValue={editTask.content}
+                value={editTask.content} 
+                onChange={(e) => setEditTask({...editTask, content: e.target.value})} 
+              />
+            </div>
+            <div className='grid grid-cols-2 gap-4'>
+              <Button type="submit" variant="outline">保存</Button>
+              <DialogClose asChild>
+                <Button type="button" variant="outline">关闭</Button>
+              </DialogClose>
+            </div>
+          </form>
+        </DialogContent>
+      </Dialog>
+
+      <Command className='flex flex-col gap-2'>
+        <CommandInput placeholder="搜索任务" />
+        <CommandList>
+          <CommandEmpty>未找到匹配任务</CommandEmpty>
+          {tasks.filter(task => filter === 'all' || (filter === 'done' && task.done) || (filter === 'undone' && !task.done)).map((task, index) => (
+            <CommandItem key={index} className='mt-0'>
+              <Checkbox className="mr-2" checked={task.done} onCheckedChange={() => {switchTask(task.id)}} />  
+              <Label className={task.done? "line-through": ""}>
+                {task.title}
+              </Label>
+              <CommandShortcut className="h-full flex flex-row items-center gap-0">
+                <Button variant="ghost" onClick={() => {setEditTask(task) ;setOpen(true)}}>
+                  <PenLine className="mr-2 h-6 w-6" />
+                </Button>
+                <Button variant="ghost" onClick={() => deleteTask(task.id)}>
+                  <Trash2 className="h-6 w-6" />
+                </Button>
+              </CommandShortcut>
+            </CommandItem>
+          ))}
+        </CommandList>
+      </Command>
+
     </div>
   );
 }
+
+
+
+
